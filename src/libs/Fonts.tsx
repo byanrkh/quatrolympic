d7cc326858edfbdfad1cffdafed8ba8b1fbd0f10
byanrkh <abyan.raditya@icloud.com>
2026-07-02 22:33:37 +0700

    QUATROLYMPIC

---
+++ src/libs/Fonts.tsx
@@ -0,0 +1,15 @@
+import { Space_Grotesk, Space_Mono } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+const spaceMono = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-space-mono",
+});
+
+export { spaceGrotesk, spaceMono };
